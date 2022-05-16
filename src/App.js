ffab01008e4cd0ab30365218ed74f27bc960f59e
uyriq <105006434+uyriq@users.noreply.github.com>
2022-05-17 00:30:57 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,60 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import styles from './styles.module.css';
+import { SwitchControl } from './components/SwitchControl';
+import { FlowControl, FLOW_OPTIONS } from './components/FlowControl';
+import { MainDashboard, MIN_TEMPERATURE, MAX_TEMPERATURE } from './components/MainDashboard';
 
-export default class App extends React.Component {}
+export default class App extends React.Component {
+  state = {
+    enabled: false,
+    temperature: 21,
+    flow: 1
+  };
+
+  /* Здесь ваш код */
+
+  render() {
+    const { enabled } = this.state;
+
+    return (
+      <div className={styles.root}>
+        <div className={styles.wrap}>
+          <h1 className={styles.title}>Гостиная</h1>
+          <div className={styles.card}>
+            <div className={styles.column}>
+              <SwitchControl enabled={enabled} onClick={/* Здесь ваш код */} />
+              <div>
+                <span className={styles.iconFan} />
+                <label>
+                  Скорость обдува
+                  <div className={styles.fanRow}>
+                    {FLOW_OPTIONS.map(elem => (
+                      <FlowControl
+                        key={`flow_elem${elem}`}
+                        flow={elem}
+                        selectedFlow={this.state.flow}
+                        onClick={/* Здесь ваш код */}
+                      />
+                    ))}
+                  </div>
+                </label>
+              </div>
+            </div>
+            <MainDashboard
+              temperature={this.state.temperature}
+              onIncreaseClick={/* Здесь ваш код */}
+              onDecreaseClick={/* Здесь ваш код */}
+            />
+            <div className={styles.column}>
+              <span className={styles.iconDrop} />
+              <label className={styles.dropLabel}>
+                Влажность
+                <p>52%</p>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
